a343d9062831efef10b8a6752fc2f5b011a374d9
Joel Höner <athre0z@zyantific.com>
2018-05-21 22:07:53 +0200

    Fixed build on CentOS / RHEL
    or, more generally speaking, any toolkit that doesn't enable C99 support by default
    
    Closes #40

--- include/Zydis/Defines.h
+++ include/Zydis/Defines.h
@@ -170,7 +170,10 @@
 #if __STDC_VERSION__ >= 201112L
 #   define ZYDIS_STATIC_ASSERT(x) _Static_assert(x, #x)
 #else
-#   define ZYDIS_STATIC_ASSERT(x) typedef int ZYDIS_SASSERT_IMPL[(x) ? 1 : -1]
+#   define ZYDIS_MACRO_CONCAT2(x, y) x##y
+#   define ZYDIS_MACRO_CONCAT(x, y) ZYDIS_MACRO_CONCAT2(x, y)
+#   define ZYDIS_STATIC_ASSERT(x) \
+        typedef int ZYDIS_MACRO_CONCAT(ZYDIS_SASSERT_, __COUNTER__) [(x) ? 1 : -1]
 #endif
 
 /**
