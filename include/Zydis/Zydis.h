bfee99f49274a0eec3ffea16ede3a5bda9cda88f
Joel Höner <athre0z@zyantific.com>
2020-02-24 15:49:18 +0100

    Bump version to v3.1

--- include/Zydis/Zydis.h
+++ include/Zydis/Zydis.h
@@ -66,7 +66,7 @@ extern "C" {
 /**
  * @brief   A macro that defines the zydis version.
  */
-#define ZYDIS_VERSION (ZyanU64)0x0003000000000000
+#define ZYDIS_VERSION (ZyanU64)0x0003000100000000
 
 /* ---------------------------------------------------------------------------------------------- */
 /* Helper macros                                                                                  */
