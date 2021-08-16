fe8f1bfd07c660a12284223acd6696728f217449
mappzor <34216645+mappzor@users.noreply.github.com>
2021-08-16 19:46:46 +0200

    Enhanced formatting for MIB operands

--- src/FormatterATT.c
+++ src/FormatterATT.c
@@ -211,10 +211,9 @@ ZyanStatus ZydisFormatterATTFormatOperandMEM(const ZydisFormatter* formatter,
         ZYAN_CHECK(formatter->func_print_address_abs(formatter, buffer, context));
     } else
     {
-        ZyanBool should_print_reg = context->operand->mem.base != ZYDIS_REGISTER_NONE;
-        ZyanBool should_print_idx = (context->operand->mem.index != ZYDIS_REGISTER_NONE) &&
-            (context->operand->mem.type != ZYDIS_MEMOP_TYPE_MIB);
-        ZyanBool neither_reg_nor_idx = !should_print_reg && !should_print_idx;
+        const ZyanBool should_print_reg = context->operand->mem.base != ZYDIS_REGISTER_NONE;
+        const ZyanBool should_print_idx = context->operand->mem.index != ZYDIS_REGISTER_NONE;
+        const ZyanBool neither_reg_nor_idx = !should_print_reg && !should_print_idx;
 
         // Regular memory operand
         if (neither_reg_nor_idx)
@@ -242,7 +241,7 @@ ZyanStatus ZydisFormatterATTFormatOperandMEM(const ZydisFormatter* formatter,
             ZYDIS_BUFFER_APPEND(buffer, DELIM_MEMORY);
             ZYAN_CHECK(formatter->func_print_register(formatter, buffer, context,
                 context->operand->mem.index));
-            if (context->operand->mem.scale)
+            if (context->operand->mem.scale && context->operand->mem.type != ZYDIS_MEMOP_TYPE_MIB)
             {
                 ZYDIS_BUFFER_APPEND_TOKEN(buffer, ZYDIS_TOKEN_DELIMITER);
                 ZYDIS_BUFFER_APPEND(buffer, DELIM_MEMORY);
