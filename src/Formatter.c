55dd08c210722aed81b38132f5fd4a04ec1943b5
Joel Höner <athre0z@zyantific.com>
2021-10-24 14:36:42 +0200

    Fix struct initialization in formatter

--- src/Formatter.c
+++ src/Formatter.c
@@ -62,13 +62,18 @@ void ZydisFormatterBufferInit(ZydisFormatterBuffer* buffer, char* user_buffer,
     ZYAN_ASSERT(user_buffer);
     ZYAN_ASSERT(length);
 
-    buffer->is_token_list              = ZYAN_FALSE;
-    buffer->string.flags               = ZYAN_STRING_HAS_FIXED_CAPACITY;
-    buffer->string.vector.allocator    = ZYAN_NULL;
-    buffer->string.vector.element_size = sizeof(char);
-    buffer->string.vector.size         = 1;
-    buffer->string.vector.capacity     = length;
-    buffer->string.vector.data         = user_buffer;
+    buffer->is_token_list                   = ZYAN_FALSE;
+    buffer->capacity                        = 0;
+    buffer->string.flags                    = ZYAN_STRING_HAS_FIXED_CAPACITY;
+    buffer->string.vector.allocator         = ZYAN_NULL;
+    buffer->string.vector.growth_factor     = 1;
+    buffer->string.vector.shrink_threshold  = 0;
+    buffer->string.vector.destructor        = ZYAN_NULL;
+    buffer->string.vector.element_size      = sizeof(char);
+    buffer->string.vector.size              = 1;
+    buffer->string.vector.capacity          = length;
+    buffer->string.vector.data              = user_buffer;
+
     *user_buffer = '\0';
 }
 
@@ -87,14 +92,18 @@ void ZydisFormatterBufferInitTokenized(ZydisFormatterBuffer* buffer,
     user_buffer = (ZyanU8*)user_buffer + sizeof(ZydisFormatterToken);
     length -= sizeof(ZydisFormatterToken);
 
-    buffer->is_token_list              = ZYAN_TRUE;
-    buffer->capacity                   = length;
-    buffer->string.flags               = ZYAN_STRING_HAS_FIXED_CAPACITY;
-    buffer->string.vector.allocator    = ZYAN_NULL;
-    buffer->string.vector.element_size = sizeof(char);
-    buffer->string.vector.size         = 1;
-    buffer->string.vector.capacity     = length;
-    buffer->string.vector.data         = user_buffer;
+    buffer->is_token_list                  = ZYAN_TRUE;
+    buffer->capacity                       = length;
+    buffer->string.flags                   = ZYAN_STRING_HAS_FIXED_CAPACITY;
+    buffer->string.vector.allocator        = ZYAN_NULL;
+    buffer->string.vector.growth_factor    = 1;
+    buffer->string.vector.shrink_threshold = 0;
+    buffer->string.vector.destructor       = ZYAN_NULL;
+    buffer->string.vector.element_size     = sizeof(char);
+    buffer->string.vector.size             = 1;
+    buffer->string.vector.capacity         = length;
+    buffer->string.vector.data             = user_buffer;
+
     *(char*)user_buffer = '\0';
 }
 
