4eff14361447d27486b368518b13b7776aa2c56e
Joel Höner <athre0z@zyantific.com>
2022-06-05 19:21:30 +0200

    Decoder: simplify VEX parsing expressions

--- src/Decoder.c
+++ src/Decoder.c
@@ -458,28 +458,28 @@ static ZyanStatus ZydisDecodeVEX(ZydisDecoderContext* context,
     case 0xC4:
         ZYAN_ASSERT(instruction->raw.vex.offset == instruction->length - 3);
         instruction->raw.vex.size    = 3;
-        context->vector_unified.R    = 0x01 & ~((data[1] >> 7) & 0x01);
-        context->vector_unified.X    = 0x01 & ~((data[1] >> 6) & 0x01);
-        context->vector_unified.B    = 0x01 & ~((data[1] >> 5) & 0x01);
-        instruction->raw.vex.m_mmmm  = (data[1] >> 0) & 0x1F;
-        context->vector_unified.W    = (data[2] >> 7) & 0x01;
-        context->vector_unified.vvvv = (0x0F & ~((data[2] >> 3) & 0x0F));
-        context->vector_unified.L    = (data[2] >> 2) & 0x01;
+        context->vector_unified.R    = 0x01 & ~(data[1] >> 7);
+        context->vector_unified.X    = 0x01 & ~(data[1] >> 6);
+        context->vector_unified.B    = 0x01 & ~(data[1] >> 5);
+        instruction->raw.vex.m_mmmm  = 0x1F &  (data[1] >> 0);
+        context->vector_unified.W    = 0x01 &  (data[2] >> 7);
+        context->vector_unified.vvvv = 0x0F & ~(data[2] >> 3);
+        context->vector_unified.L    = 0x01 &  (data[2] >> 2);
         context->vector_unified.LL   = context->vector_unified.L;
-        instruction->raw.vex.pp      = (data[2] >> 0) & 0x03;
+        instruction->raw.vex.pp      = 0x03 &  (data[2] >> 0);
         break;
     case 0xC5:
         ZYAN_ASSERT(instruction->raw.vex.offset == instruction->length - 2);
         instruction->raw.vex.size    = 2;
-        context->vector_unified.R    = 0x01 & ~((data[1] >> 7) & 0x01);
-        context->vector_unified.X    = 0x01 & ~1;
-        context->vector_unified.B    = 0x01 & ~1;
+        context->vector_unified.R    = 0x01 & ~(data[1] >> 7);
+        context->vector_unified.X    = 0;
+        context->vector_unified.B    = 0;
         instruction->raw.vex.m_mmmm  = 1;
         context->vector_unified.W    = 0;
-        context->vector_unified.vvvv = (0x0F & ~((data[1] >> 3) & 0x0F));
-        context->vector_unified.L    = (data[1] >> 2) & 0x01;
+        context->vector_unified.vvvv = 0x0F & ~(data[1] >> 3);
+        context->vector_unified.L    = 0x01 &  (data[1] >> 2);
         context->vector_unified.LL   = context->vector_unified.L;
-        instruction->raw.vex.pp      = (data[1] >> 0) & 0x03;
+        instruction->raw.vex.pp      = 0x03 &  (data[1] >> 0);
         break;
     default:
         ZYAN_UNREACHABLE;
