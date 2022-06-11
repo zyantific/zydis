a8ee8aa4b7e5a603eda09a096a3d7c6b6929f901
Joel Höner <athre0z@zyantific.com>
2022-06-11 15:53:01 +0200

    use reflection info for decoding segments

--- src/Segment.c
+++ src/Segment.c
@@ -31,129 +31,75 @@
 /* Helpers                                                                                        */
 /* ============================================================================================== */
 
-ZYAN_INLINE ZydisInstructionSegmentNoBits ZydisSegmentDecodeNoBits(void)
-{
-    return (ZydisInstructionSegmentNoBits) {};
-}
+/**
+ * Keeps context between `ZydisAddSegment` calls.
+ */
+typedef struct ZydisSegmentContext_ {
+    /**
+     * Pointer to the first byte of the instruction.
+     */
+    const ZyanU8* const raw_instr;
+    /**
+     * Length of `raw_instr`.
+     */
+    const ZyanU8 instr_length;
+    /**
+     * Output buffer to write segments to.
+     */
+    ZydisInstructionSegments *const segments;
+    /**
+     * Current byte offset within the instruction.
+     */
+    ZyanU8 offset;
+} ZydisSegmentContext;
 
-ZYAN_INLINE ZydisInstructionSegmentREX ZydisSegmentDecodeREX(const ZyanU8 rex)
+ZyanStatus ZydisAddSegment(ZydisSegmentContext* ctx, ZydisInstructionSegmentKind kind, ZyanU8 size)
 {
-    return (ZydisInstructionSegmentREX)
-    {
-        .W = 0x01 & (rex >> 3),
-        .R = 0x01 & (rex >> 2),
-        .X = 0x01 & (rex >> 1),
-        .B = 0x01 & (rex >> 0),
-    };
-}
+    ZYAN_ASSERT(ctx);
+    ZYAN_ASSERT(kind <= ZYDIS_INSTR_SEGMENT_MAX_VALUE);
+    ZYAN_ASSERT(size > 0 && size < ZYDIS_MAX_INSTRUCTION_LENGTH - 1);
+    ZYAN_ASSERT(ctx->segments->count < ZYAN_ARRAY_LENGTH(ctx->segments->segments));
 
-ZYAN_INLINE ZydisInstructionSegmentVEX ZydisSegmentDecodeVEX(
-    const ZyanU8* const vex, const ZyanU8 length)
-{
-    switch (length)
+    if ((ZyanU32)(ctx->offset) + (ZyanU32)size > ctx->instr_length)
     {
-    case 2:
-        return (ZydisInstructionSegmentVEX)
-        {
-            .R       = 0x01 & (vex[1] >> 7),
-            .X       = 1,
-            .B       = 1,
-            .m_mmmm  = 1,
-            .W       = 0,
-            .vvvv    = 0x0F & (vex[1] >> 3),
-            .L       = 0x01 & (vex[1] >> 2),
-            .pp      = 0x03 & (vex[1] >> 0),
-        };
-    case 3:
-        return (ZydisInstructionSegmentVEX)
-        {
-            .R       = 0x01 & (vex[1] >> 7),
-            .X       = 0x01 & (vex[1] >> 6),
-            .B       = 0x01 & (vex[1] >> 5),
-            .m_mmmm  = 0x1F & (vex[1] >> 0),
-            .W       = 0x01 & (vex[2] >> 7),
-            .vvvv    = 0x0F & (vex[2] >> 3),
-            .L       = 0x01 & (vex[2] >> 2),
-            .pp      = 0x03 & (vex[2] >> 0),
-        };
-    default:
-        ZYAN_UNREACHABLE
+        return ZYDIS_STATUS_NO_MORE_DATA;
     }
-}
 
-ZYAN_INLINE ZydisInstructionSegmentXOP ZydisSegmentDecodeXOP(const ZyanU8* const xop)
-{
-    return (ZydisInstructionSegmentXOP)
-    {
-        .R      = 0x01 & (xop[1] >> 7),
-        .X      = 0x01 & (xop[1] >> 6),
-        .B      = 0x01 & (xop[1] >> 5),
-        .m_mmmm = 0x1F & (xop[1] >> 0),
-        .W      = 0x01 & (xop[2] >> 7),
-        .vvvv   = 0x0F & (xop[2] >> 3),
-        .L      = 0x01 & (xop[2] >> 2),
-        .pp     = 0x03 & (xop[2] >> 0),
-    };
-}
+    ZydisInstructionSegment* seg = &ctx->segments->segments[ctx->segments->count++];
 
-ZYAN_INLINE ZydisInstructionSegmentEVEX ZydisSegmentDecodeEVEX(const ZyanU8* evex)
-{
-    return (ZydisInstructionSegmentEVEX)
-    {
-        .R     = 0x01 & (evex[1] >> 7),
-        .X     = 0x01 & (evex[1] >> 6),
-        .B     = 0x01 & (evex[1] >> 5),
-        .R2    = 0x01 & (evex[1] >> 4),
-        .mmm   = 0x07 & (evex[1] >> 0),
-        .W     = 0x01 & (evex[2] >> 7),
-        .vvvv  = 0x0F & (evex[2] >> 3),
-        .pp    = 0x03 & (evex[2] >> 0),
-        .z     = 0x01 & (evex[3] >> 7),
-        .L2    = 0x01 & (evex[3] >> 6),
-        .L     = 0x01 & (evex[3] >> 5),
-        .b     = 0x01 & (evex[3] >> 4),
-        .V2    = 0x01 & (evex[3] >> 3),
-        .aaa   = 0x07 & (evex[3] >> 0),
+    *seg = (ZydisInstructionSegment) {
+        .type = kind,
+        .size = size,
+        .offset = ctx->offset,
     };
-}
 
-ZYAN_INLINE ZydisInstructionSegmentMVEX ZydisSegmentDecodeMVEX(const ZyanU8* mvex)
-{
-    return (ZydisInstructionSegmentMVEX)
-    {
-        .R    = 0x01 & (mvex[1] >> 7),
-        .X    = 0x01 & (mvex[1] >> 6),
-        .B    = 0x01 & (mvex[1] >> 5),
-        .R2   = 0x01 & (mvex[1] >> 4),
-        .mmmm = 0x0F & (mvex[1] >> 0),
-        .W    = 0x01 & (mvex[2] >> 7),
-        .vvvv = 0x0F & (mvex[2] >> 3),
-        .pp   = 0x03 & (mvex[2] >> 0),
-        .E    = 0x01 & (mvex[3] >> 7),
-        .SSS  = 0x07 & (mvex[3] >> 4),
-        .V2   = 0x01 & (mvex[3] >> 3),
-        .kkk  = 0x07 & (mvex[3] >> 0),
-    };
-}
+    const ZydisInstructionSegmentReflectionInfo *info =
+        ZydisSegmentGetReflectionInfo(kind, size);
+    ZYAN_ASSERT(info); // the internal code should never call with bad arguments
 
-ZYAN_INLINE ZydisInstructionSegmentModRM ZydisSegmentDecodeModRM(const ZyanU8 modrm)
-{
-    return (ZydisInstructionSegmentModRM)
+    for (; info->field_name; ++info)
     {
-        .mod = 0x03 & (modrm >> 6),
-        .reg = 0x07 & (modrm >> 3),
-        .rm  = 0x07 & (modrm >> 0),
-    };
-}
+        // `prefix` was picked arbitrarily here: we can use any union variant.
+        ZyanU8* field = ((ZyanU8*)&seg->prefix) + info->struct_offset;
 
-ZYAN_INLINE ZydisInstructionSegmentSIB ZydisSegmentDecodeSIB(const ZyanU8 sib)
-{
-    return (ZydisInstructionSegmentSIB)
-    {
-        .scale = 0x03 & (sib >> 6),
-        .index = 0x07 & (sib >> 3),
-        .base  = 0x07 & (sib >> 0),
-    };
+        if (info->bit_width == 0)
+        {
+            // Constant.
+            *field = info->value_source.constant;
+        } else
+        {
+            // Read from raw segment data.
+            ZyanU8 byte = info->value_source.bit_offset / 8;
+            ZyanU8 bit  = info->value_source.bit_offset % 8;
+            ZyanU8 mask = (1 << info->bit_width) - 1;
+            ZYAN_ASSERT(byte < size);
+            *field = (ctx->raw_instr[ctx->offset + byte] >> bit) & mask;
+        }
+    }
+
+    ctx->offset += size;
+
+    return ZYAN_STATUS_SUCCESS;
 }
 
 /* ============================================================================================== */
@@ -168,14 +114,14 @@ ZyanStatus ZydisGetInstructionSegments(const ZydisDecodedInstruction* const inst
         return ZYAN_STATUS_INVALID_ARGUMENT;
     }
 
-#   define ZYDIS_CHECK_AND_ADVANCE_BUFFER(n)                    \
-        do {                                                    \
-            if (length < (n)) return ZYDIS_STATUS_NO_MORE_DATA; \
-            buffer += n;                                        \
-            length -= n;                                        \
-        } while (0)
-
     segments->count = 0;
+
+    ZydisSegmentContext ctx = {
+        .offset = 0,
+        .segments = segments,
+        .raw_instr = buffer,
+        .instr_length = length,
+    };
 
     // Legacy prefixes and `REX`.
     if (instruction->raw.prefix_count)
@@ -183,25 +129,14 @@ ZyanStatus ZydisGetInstructionSegments(const ZydisDecodedInstruction* const inst
         const ZyanU8 rex_offset = (instruction->attributes & ZYDIS_ATTRIB_HAS_REX) ? 1 : 0;
         if (!rex_offset || (instruction->raw.prefix_count > 1))
         {
-            ZYDIS_CHECK_AND_ADVANCE_BUFFER(instruction->raw.prefix_count - rex_offset);
-            segments->segments[segments->count++] = (ZydisInstructionSegment)
-            {
-                .offset  = 0,
-                .size    = instruction->raw.prefix_count - rex_offset,
-                .type    = ZYDIS_INSTR_SEGMENT_PREFIXES,
-                .prefix  = ZydisSegmentDecodeNoBits(),
-            };
+            ZYAN_ASSERT(ctx.offset == 0);
+            ZYAN_CHECK(ZydisAddSegment(&ctx, ZYDIS_INSTR_SEGMENT_PREFIXES,
+                instruction->raw.prefix_count - rex_offset));
         }
         if (rex_offset)
         {
-            ZYDIS_CHECK_AND_ADVANCE_BUFFER(1);
-            segments->segments[segments->count++] = (ZydisInstructionSegment)
-            {
-                .offset = instruction->raw.prefix_count - rex_offset,
-                .size   = 1,
-                .type   = ZYDIS_INSTR_SEGMENT_REX,
-                .rex    = ZydisSegmentDecodeREX(*buffer),
-            };
+            ZYAN_ASSERT(ctx.offset == instruction->raw.rex.offset);
+            ZYAN_CHECK(ZydisAddSegment(&ctx, ZYDIS_INSTR_SEGMENT_REX, 1));
         }
     }
 
@@ -209,44 +144,20 @@ ZyanStatus ZydisGetInstructionSegments(const ZydisDecodedInstruction* const inst
     switch (instruction->encoding)
     {
     case ZYDIS_INSTRUCTION_ENCODING_XOP:
-        ZYDIS_CHECK_AND_ADVANCE_BUFFER(3);
-        segments->segments[segments->count++] = (ZydisInstructionSegment)
-        {
-            .offset = instruction->raw.xop.offset,
-            .size   = 3,
-            .type   = ZYDIS_INSTR_SEGMENT_XOP,
-            .xop    = ZydisSegmentDecodeXOP(buffer),
-        };
+        ZYAN_ASSERT(ctx.offset == instruction->raw.xop.offset);
+        ZYAN_CHECK(ZydisAddSegment(&ctx, ZYDIS_INSTR_SEGMENT_XOP, 3));
         break;
     case ZYDIS_INSTRUCTION_ENCODING_VEX:
-        ZYDIS_CHECK_AND_ADVANCE_BUFFER(instruction->raw.vex.size);
-        segments->segments[segments->count++] = (ZydisInstructionSegment)
-        {
-            .offset = instruction->raw.vex.offset,
-            .size   = instruction->raw.vex.size,
-            .type   = ZYDIS_INSTR_SEGMENT_VEX,
-            .vex    = ZydisSegmentDecodeVEX(buffer, instruction->raw.vex.size),
-        };
+        ZYAN_ASSERT(ctx.offset == instruction->raw.vex.offset);
+        ZYAN_CHECK(ZydisAddSegment(&ctx, ZYDIS_INSTR_SEGMENT_VEX, instruction->raw.vex.size));
         break;
     case ZYDIS_INSTRUCTION_ENCODING_EVEX:
-        ZYDIS_CHECK_AND_ADVANCE_BUFFER(4);
-        segments->segments[segments->count++] = (ZydisInstructionSegment)
-        {
-            .offset = instruction->raw.evex.offset,
-            .size   = 4,
-            .type   = ZYDIS_INSTR_SEGMENT_EVEX,
-            .evex   = ZydisSegmentDecodeEVEX(buffer),
-        };
+        ZYAN_ASSERT(ctx.offset == instruction->raw.evex.offset);
+        ZYAN_CHECK(ZydisAddSegment(&ctx, ZYDIS_INSTR_SEGMENT_EVEX, 4));
         break;
     case ZYDIS_INSTRUCTION_ENCODING_MVEX:
-        ZYDIS_CHECK_AND_ADVANCE_BUFFER(4);
-        segments->segments[segments->count++] = (ZydisInstructionSegment)
-        {
-            .offset = instruction->raw.mvex.offset,
-            .size   = 4,
-            .type   = ZYDIS_INSTR_SEGMENT_MVEX,
-            .mvex   = ZydisSegmentDecodeMVEX(buffer),
-        };
+        ZYAN_ASSERT(ctx.offset == instruction->raw.mvex.offset);
+        ZYAN_CHECK(ZydisAddSegment(&ctx, ZYDIS_INSTR_SEGMENT_MVEX, 4));
         break;
     default:
         break;
@@ -276,60 +187,28 @@ ZyanStatus ZydisGetInstructionSegments(const ZydisDecodedInstruction* const inst
         }
     }
 
-    ZyanU8 opcode_offset = 0;
-    if (segments->count)
-    {
-        const ZydisInstructionSegment * const prev =
-            &segments->segments[segments->count - 1];
-        opcode_offset = prev->offset + prev->size;
-    }
-
-    ZYDIS_CHECK_AND_ADVANCE_BUFFER(opcode_size);
-    segments->segments[segments->count++] = (ZydisInstructionSegment)
-    {
-        .offset = opcode_offset,
-        .size   = opcode_size,
-        .type   = ZYDIS_INSTR_SEGMENT_OPCODE,
-        .opcode = ZydisSegmentDecodeNoBits(),
-    };
+    ZYAN_CHECK(ZydisAddSegment(&ctx, ZYDIS_INSTR_SEGMENT_OPCODE, opcode_size));
 
     // ModRM
     if (instruction->attributes & ZYDIS_ATTRIB_HAS_MODRM)
     {
-        ZYDIS_CHECK_AND_ADVANCE_BUFFER(1);
-        segments->segments[segments->count++] = (ZydisInstructionSegment)
-        {
-            .offset = instruction->raw.modrm.offset,
-            .size   = 1,
-            .type   = ZYDIS_INSTR_SEGMENT_MODRM,
-            .modrm  = ZydisSegmentDecodeModRM(*buffer),
-        };
+        ZYAN_ASSERT(ctx.offset == instruction->raw.modrm.offset);
+        ZYAN_CHECK(ZydisAddSegment(&ctx, ZYDIS_INSTR_SEGMENT_MODRM, 1));
     }
 
     // SIB
     if (instruction->attributes & ZYDIS_ATTRIB_HAS_SIB)
     {
-        ZYDIS_CHECK_AND_ADVANCE_BUFFER(1);
-        segments->segments[segments->count++] = (ZydisInstructionSegment)
-        {
-            .offset = instruction->raw.sib.offset,
-            .size   = 1,
-            .type   = ZYDIS_INSTR_SEGMENT_SIB,
-            .sib    = ZydisSegmentDecodeSIB(*buffer),
-        };
+        ZYAN_ASSERT(ctx.offset == instruction->raw.sib.offset);
+        ZYAN_CHECK(ZydisAddSegment(&ctx, ZYDIS_INSTR_SEGMENT_SIB, 1));
     }
 
     // Displacement
     if (instruction->raw.disp.size)
     {
-        ZYDIS_CHECK_AND_ADVANCE_BUFFER(instruction->raw.disp.size / 8);
-        segments->segments[segments->count++] = (ZydisInstructionSegment)
-        {
-            .offset = instruction->raw.disp.offset,
-            .size   = instruction->raw.disp.size / 8,
-            .type   = ZYDIS_INSTR_SEGMENT_DISPLACEMENT,
-            .disp   = ZydisSegmentDecodeNoBits(),
-        };
+        ZYAN_ASSERT(ctx.offset == instruction->raw.disp.offset);
+        ZYAN_CHECK(ZydisAddSegment(&ctx, ZYDIS_INSTR_SEGMENT_DISPLACEMENT,
+            instruction->raw.disp.size / 8));
     }
 
     // Immediates
@@ -337,35 +216,21 @@ ZyanStatus ZydisGetInstructionSegments(const ZydisDecodedInstruction* const inst
     {
         if (instruction->raw.imm[i].size)
         {
-            ZYDIS_CHECK_AND_ADVANCE_BUFFER(instruction->raw.imm[i].size / 8);
-            segments->segments[segments->count++] = (ZydisInstructionSegment)
-            {
-                .offset = instruction->raw.imm[i].offset,
-                .size   = instruction->raw.imm[i].size / 8,
-                .type   = ZYDIS_INSTR_SEGMENT_IMMEDIATE,
-                .imm    = ZydisSegmentDecodeNoBits(),
-            };
+            ZYAN_ASSERT(ctx.offset == instruction->raw.imm[i].offset);
+            ZYAN_CHECK(ZydisAddSegment(&ctx, ZYDIS_INSTR_SEGMENT_IMMEDIATE,
+                instruction->raw.imm[i].size / 8));
         }
     }
 
     // 3DNow! suffix opcode
     if (instruction->encoding == ZYDIS_INSTRUCTION_ENCODING_3DNOW)
     {
-        ZYDIS_CHECK_AND_ADVANCE_BUFFER(1);
-        segments->segments[segments->count++] = (ZydisInstructionSegment)
-        {
-            .offset = instruction->length - 1,
-            .size   = 1,
-            .type   = ZYDIS_INSTR_SEGMENT_OPCODE,
-            .opcode = ZydisSegmentDecodeNoBits(),
-        };
+        ZYAN_CHECK(ZydisAddSegment(&ctx, ZYDIS_INSTR_SEGMENT_OPCODE, 1));
     }
 
     // Zero out remaining entries
     ZYAN_MEMSET(&segments->segments[segments->count], 0,
         sizeof(*segments->segments) - segments->count);
-
-#   undef ZYDIS_CHECK_AND_ADVANCE_BUFFER
 
     return ZYAN_STATUS_SUCCESS;
 }
