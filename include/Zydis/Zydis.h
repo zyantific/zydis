bfa662e4d506d735e0e9cf7c1a198c29a96ea7ae
Joel Höner <athre0z@zyantific.com>
2022-11-27 21:29:58 +0100

    Fix `ZydisFeature` enum max value constant

--- include/Zydis/Zydis.h
+++ include/Zydis/Zydis.h
@@ -141,7 +141,7 @@ typedef enum ZydisFeature_
     /**
      * Maximum value of this enum.
      */
-    ZYDIS_FEATURE_MAX_VALUE = ZYDIS_FEATURE_KNC,
+    ZYDIS_FEATURE_MAX_VALUE = ZYDIS_FEATURE_SEGMENT,
     /**
      * The minimum number of bits required to represent all values of this enum.
      */
