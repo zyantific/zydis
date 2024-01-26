87938cbb6e05e5485d8fc473a5a6ac4ddc5319b8
Florian Bernd <git@flobernd.de>
2024-01-26 20:34:03 +0100

    Add `UD0_COMPAT` decoder mode (#477)

--- include/Zydis/Decoder.h
+++ include/Zydis/Decoder.h
@@ -144,11 +144,20 @@ typedef enum ZydisDecoderMode_
      * This mode is enabled by default.
      */
     ZYDIS_DECODER_MODE_IPREFETCH,
+    /**
+     * Enables the `UD0` compatibility mode.
+     *
+     * Some processors decode the `UD0` instruction without a ModR/M byte. Enable this decoder mode
+     * to mimic this behavior.
+     *
+     * This mode is disabled by default.
+     */
+    ZYDIS_DECODER_MODE_UD0_COMPAT,
 
     /**
      * Maximum value of this enum.
      */
-    ZYDIS_DECODER_MODE_MAX_VALUE = ZYDIS_DECODER_MODE_IPREFETCH,
+    ZYDIS_DECODER_MODE_MAX_VALUE = ZYDIS_DECODER_MODE_UD0_COMPAT,
     /**
      * The minimum number of bits required to represent all values of this enum.
      */
