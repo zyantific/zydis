f825c74076de73a9d89534d7fea4a154154df5c9
dxbjavid <dxbjavid@gmail.com>
2026-05-25 14:31:19 +0530

    Formatter: handle FAR/ABSOLUTE in print_branch_size switch

--- src/FormatterATT.c
+++ src/FormatterATT.c
@@ -354,6 +354,8 @@ ZyanStatus ZydisFormatterATTPrintMnemonic(const ZydisFormatter* formatter,
         switch (context->instruction->meta.branch_type)
         {
         case ZYDIS_BRANCH_TYPE_NONE:
+        case ZYDIS_BRANCH_TYPE_FAR:
+        case ZYDIS_BRANCH_TYPE_ABSOLUTE:
             break;
         case ZYDIS_BRANCH_TYPE_SHORT:
             return ZydisStringAppendShortCase(&buffer->string, &STR_SHORT,
